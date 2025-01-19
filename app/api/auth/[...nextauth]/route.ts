7d439721505e8d305f0a69e0196c239c26beea4f
geetansh431 <geetanshg2@gmail.com>
2025-01-19 21:17:17 +0530

    Upvote/Stream/Downvote Endpoints

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
 import GoogleProvider from "next-auth/providers/google";
 import NextAuth from "next-auth";
+import { prismaClient } from "@/app/lib/db";
+import { NextResponse } from "next/server";
 
 const handler = NextAuth({
     providers: [
@@ -8,6 +10,25 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
         })
     ],
+    callbacks: {
+        async signIn(params) {
+            if (!params.user.email) {
+                return false;
+            }
+
+            try {
+                await prismaClient.user.create({
+                    data: {
+                        email: params.user.email,
+                        provider: "Google"
+                    } 
+                })
+             } catch(e) {
+
+             }
+            return true;
+        }
+    }
 })
 
-export { handler as GET, handler as POST}
+export { handler as GET, handler as POST }
